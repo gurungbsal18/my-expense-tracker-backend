442634d3ce8884401706b91a583c446892c2edd5
gurungbsal18 <gurungbsal18@gmail.com>
2025-04-20 02:08:10 +0545

    cors added

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ dotenv.config();
 import mongoose from "mongoose";
 import express from "express";
 import route from "./routes/route.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
