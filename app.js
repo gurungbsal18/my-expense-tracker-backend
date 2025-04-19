1e30519ebbcf0e16f1f8f630ba504cde35f6f5c2
gurungbsal18 <gurungbsal18@gmail.com>
2025-04-20 02:14:43 +0545

    cors updated

--- app.js
+++ app.js
@@ -10,13 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(
-  cors({
-    origin: "https://my-expense-tracker-gilt.vercel.app",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
